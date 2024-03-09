cf8cf73f8742c862c7abb5e8b97eae32b0a4cda4
Ryan Cueva <51128072+ryancueva282@users.noreply.github.com>
2024-03-09 15:04:30 -0600

    Update lineage.mk for tmo variant

--- lineage.mk
+++ lineage.mk
@@ -6,17 +6,17 @@ $(call inherit-product, device/samsung/gte-common/lineage.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from device.mk
-$(call inherit-product, device/samsung/gtesqltespr/device.mk)
+$(call inherit-product, device/samsung/gtesltetmo/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 $(call inherit-product, vendor/cm/config/telephony.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := gtesqltespr
-PRODUCT_NAME := lineage_gtesqltespr
+PRODUCT_DEVICE := gtesltetmo
+PRODUCT_NAME := lineage_gtesltetmo
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-T377P
+PRODUCT_MODEL := SM-T377T
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_CHARACTERISTICS := tablet
 
