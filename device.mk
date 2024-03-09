b09aedab5be52764b79a7c73f8493d0671e79eb4
Ryan Cueva <51128072+ryancueva282@users.noreply.github.com>
2024-03-09 15:00:32 -0600

    Update device.mk for tmo variant

--- device.mk
+++ device.mk
@@ -1,13 +1,13 @@
 #Inherit from vendor
-$(call inherit-product-if-exists, vendor/samsung/gtesqltespr/gtesqltespr-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/gtesltetmo/gtesltetmo-vendor.mk)
 
 # Inherit from common
 $(call inherit-product, device/samsung/gte-common/device-common.mk)
 
-LOCAL_PATH := device/samsung/gtesqltespr
+LOCAL_PATH := device/samsung/gtesltetmo
 
 # System properties
 -include $(LOCAL_PATH)/system_prop.mk
 
 # Common overlay
-DEVICE_PACKAGE_OVERLAYS += device/samsung/gtesqltespr/overlay
+DEVICE_PACKAGE_OVERLAYS += device/samsung/gtesltetmo/overlay
