01f6df67cea2c9688d996a77f0e9aeec101e3527
Ryan Cueva <51128072+ryancueva282@users.noreply.github.com>
2024-03-09 14:43:33 -0600

    Update BoardConfig.mk for tmo variant

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,10 +1,10 @@
 # Inherit from common
 include device/samsung/gte-common/BoardConfigCommon.mk
 
-LOCAL_PATH := device/samsung/gtesqltetmo
+LOCAL_PATH := device/samsung/gtesltetmo
 
 # Asserts
-TARGET_OTA_ASSERT_DEVICE := gtesqltetmo,samsung_sm_t377t,t377t
+TARGET_OTA_ASSERT_DEVICE := gtesltetmo,samsung_sm_t377t,t377t
 
 # Kernel
 TARGET_KERNEL_VARIANT_CONFIG := gtesltetmo_defconfig
